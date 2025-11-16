cf2e55576a23723d44eea34b264e92ecd1f3d325
Aneta <flowmanaged@gmail.com>
2025-11-16 17:02:19 +0100

    Fix User import in auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const User = require('../models/User');
 
 // Middleware do weryfikacji tokena JWT
 exports.protect = async (req, res, next) => {
